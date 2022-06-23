9424c84f90ba66fa142d3df16174d2bb3b940e07
benxiaohai061 <wangchuanchuanhyl@163.com>
2022-06-23 09:59:55 +0800

    Update patch.sh

--- patch.sh
+++ patch.sh
@@ -1,7 +1,7 @@
 
 cd /usr/local/tomcat/webapps/ROOT
-wget https://gitee.com/wangcc1987/tomcat/raw/master/pages/error_404.html
-wget https://gitee.com/wangcc1987/tomcat/raw/master/pages/error_500.html
+wget https://github.com/benxiaohai061/tomcat/raw/master/pages/error_404.html
+wget https://github.com/benxiaohai061/tomcat/raw/master/pages/error_500.html
 
 
 perl -i.bak -ne 'push @a, $_; if ( eof ) { print for ( @a[0 .. ($#a-1)], "<error-page>\n  <error-code>404</error-code>\n  <location>/error_404.html</location>\n</error-page>\n", $a[-1] ); }' web.xml
